72c04a941aa33cc39bceab1c75c154e12c48bdb9
MishaBeheka <46327654+MishaBeheka@users.noreply.github.com>
2020-01-22 16:50:03 +0200

    Beheka_Hw_21 (#7)
    * Setup MySQL in project
    
    * Done method get(id) in ItemDaoJdbcImpl
    
    * Done method create() in ItemDaoJdbcImpl
    
    * Done method getAll() in ItemDaoJdbcImpl
    
    * Changed method create() from PreparedStatement to Statement
    
    * Changed method getAll() from PreparedStatement to Statement
    
    * Changed method get()
    
    * Added method update()
    
    * Added method deleteById() and small fix in update()
    
    * Added method deleteByEntity() and small fix in get()
    
    * Cline project CheckStyle
    
    * Small fix method deleteByItem()

---
+++ src/main/java/mate/academy/internetshop/dao/jdbc/ItemDaoJdbcImpl.java
@@ -0,0 +1,111 @@
+package mate.academy.internetshop.dao.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+import mate.academy.internetshop.dao.ItemDao;
+import mate.academy.internetshop.lib.Dao;
+import mate.academy.internetshop.model.Item;
+import org.apache.log4j.Logger;
+
+@Dao
+public class ItemDaoJdbcImpl extends AbstractDao<Item> implements ItemDao {
+    private static Logger logger = Logger.getLogger(ItemDaoJdbcImpl.class);
+    private static String DB_NAME = "internet_shop";
+
+    public ItemDaoJdbcImpl(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public Item create(Item item) {
+        String name = item.getName();
+        double price = item.getPrice();
+        String query = String.format("INSERT INTO %s.items (name, price) VALUES('%s',%s)",
+                DB_NAME, name, price);
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            logger.warn("Item wasn't created " + e);
+        }
+        return item;
+    }
+
+    @Override
+    public Optional<Item> get(Long id) {
+        String query = String.format("SELECT * FROM %s.items WHERE item_id = %d;", DB_NAME, id);
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                long itemId = resultSet.getLong("item_id");
+                String name = resultSet.getString("name");
+                double price = resultSet.getDouble("price");
+                Item item = new Item();
+                item.setItemId(itemId);
+                item.setName(name);
+                item.setPrice(price);
+                return Optional.of(item);
+            }
+        } catch (SQLException e) {
+            logger.error("Can't get item by id " + id);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Item update(Item item) {
+        String query = String.format(Locale.ROOT,
+                "UPDATE %s.items SET name = '%s', price = %.2f WHERE item_id = %d;",
+                DB_NAME, item.getName(), item.getPrice(), item.getItemId());
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            logger.error("Item wasn't updated " + e);
+        }
+        return item;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        String query = String.format("DELETE FROM %s.items WHERE item_id = %d",
+                DB_NAME, id);
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+            return true;
+        } catch (SQLException e) {
+            logger.error("Item with id " + id + " wasn't deleted " + e);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteByEntity(Item item) {
+       return deleteById(item.getItemId());
+    }
+
+    @Override
+    public List<Item> getAll() {
+        List<Item> items = new ArrayList<>();
+        String query = String.format("SELECT * FROM %s.items", DB_NAME);
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                Item item = new Item();
+                item.setItemId(resultSet.getLong("item_id"));
+                item.setName(resultSet.getString("name"));
+                item.setPrice(resultSet.getDouble("price"));
+                items.add(item);
+            }
+
+        } catch (SQLException e) {
+            logger.info("Can't show items " + e);
+        }
+        return items;
+    }
+}
--- src/main/java/mate/academy/internetshop/factory/DaoFactory.java
+++ src/main/java/mate/academy/internetshop/factory/DaoFactory.java
@@ -1,13 +1,17 @@
 package mate.academy.internetshop.factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import mate.academy.internetshop.dao.BucketDao;
 import mate.academy.internetshop.dao.ItemDao;
 import mate.academy.internetshop.dao.OrderDao;
 import mate.academy.internetshop.dao.UserDao;
 import mate.academy.internetshop.dao.implementation.BucketDaoImpl;
-import mate.academy.internetshop.dao.implementation.ItemDaoImpl;
 import mate.academy.internetshop.dao.implementation.OrderDaoImpl;
 import mate.academy.internetshop.dao.implementation.UserDaoImpl;
+import mate.academy.internetshop.dao.jdbc.ItemDaoJdbcImpl;
 import mate.academy.internetshop.service.BucketService;
 import mate.academy.internetshop.service.ItemService;
 import mate.academy.internetshop.service.OrderService;
@@ -16,9 +20,10 @@ import mate.academy.internetshop.service.implementation.BucketServiceImpl;
 import mate.academy.internetshop.service.implementation.ItemServiceImpl;
 import mate.academy.internetshop.service.implementation.OrderServiceImpl;
 import mate.academy.internetshop.service.implementation.UserServiceImpl;
+import org.apache.log4j.Logger;
 
 public class DaoFactory {
-
+    private static Logger logger = Logger.getLogger(DaoFactory.class);
     private static UserDao userDao;
     private static BucketDao bucketDao;
     private static ItemDao itemDao;
@@ -27,6 +32,19 @@ public class DaoFactory {
     private static OrderService orderService;
     private static ItemService itemService;
     private static BucketService bucketService;
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection =
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/internet_shop?"
+                            + "user=root&password=MySQL101101&serverTimezone=UTC");
+        } catch (ClassNotFoundException | SQLException e) {
+            logger.error("Can't establish connection to DB", e);
+        }
+
+    }
 
     public static UserDao getUserDao() {
         if (userDao == null) {
@@ -44,7 +62,7 @@ public class DaoFactory {
 
     public static ItemDao getItemDao() {
         if (itemDao == null) {
-            itemDao = new ItemDaoImpl();
+            itemDao = new ItemDaoJdbcImpl(connection);
         }
         return itemDao;
     }
